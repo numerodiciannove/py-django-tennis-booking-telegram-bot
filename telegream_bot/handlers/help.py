ca698315eb0ddb4d4d07ff5de3821cc5b24cc32f
numer <5079447@gmail.com>
2024-05-04 18:29:28 +0300

    Updated

--- telegream_bot/handlers/help.py
+++ telegream_bot/handlers/help.py
@@ -1,26 +1,28 @@
 from aiogram import Bot
 from aiogram.types import Message
-from telegream_bot.keyboards.profile_kb import profile_kb
 
 
 async def get_help(message: Message, bot: Bot):
-        help_text = (
-            f"Привіт!,👋 {message.from_user.full_name}!\n"
-            f"Мене звати 'PO 🎾 TENNIS BOT'.\n\n"
-            f"Я бот який допомогає робити бронювання "
-            f"спорт локації в 'ЖК Паркові Озера' міста Київ. \n\n"
-            f"В мене є декілька особливостей з позначками:\n\n"
-            f"🎾💪: Персональне тренування\n"
-            f"🎾👶👧🏻🧒🏾: Тенісна група для дітей\n"
-            f"🎾🧍‍♂️🧍‍♀️🧍: Тенісна група для дорослих\n"
-            f"🎾🏆: Тенісний турнір\n"
-            f"🏐: Волейбол\n"
-            f"🏀: Баскетбол\n\n"
-            f"Основний чат бота: @Nikitarista\n"
-            f"Якшо є бажання залишити донат розробнику @numerodiciannove:\n"
-            f"Monobank: 4441 1144 5035 4715\n\n"
-            f"Також всі технічні питання до @numerodiciannove."
-        )
-        await bot.send_message(
-            message.from_user.id, help_text,
-        )
+    help_text = (
+        f"Привіт!,👋 {message.from_user.full_name}!\n"
+        f"Мене звати 'PO 🎾 TENNIS BOT'.\n\n"
+        f"Я бот який допомогає робити бронювання "
+        f"спорт локації в 'ЖК Паркові Озера' міста Київ. \n\n"
+        f"В мене є декілька особливостей з позначками:\n\n"
+        f"🎾💪: Персональне тренування\n"
+        f"🎾👶👧🏻🧒🏾: Тенісна група для дітей\n"
+        f"🎾🧍‍♂️🧍‍♀️🧍: Тенісна група для дорослих\n"
+        f"🎾🏆: Тенісний турнір\n"
+        f"🏐: Волейбол\n"
+        f"🏀: Баскетбол\n\n"
+        f"Основний чат бота:\n"
+        f"https://t.me/+PmZ69lpeKZBhZmMy\n"
+        f"Адмін:\n"
+        f"@Nikitarista\n\n"
+        f"Якшо є бажання залишити донат розробнику @numerodiciannove:\n"
+        f"Monobank: 4441 1144 5035 4715\n\n"
+        f"Також всі технічні питання до @numerodiciannove."
+    )
+    await bot.send_message(
+        message.from_user.id, help_text,
+    )
